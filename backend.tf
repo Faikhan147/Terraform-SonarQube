d81af5251aeb1702ff205fc0563b180e78c4913d
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-19 08:03:56 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-backend-faisal-khan"
-    key            = "jenkins/terraform.tfstate"
+    key            = "sonarqube/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
   }

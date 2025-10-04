a459ac4249dbc8627c36d62e11027912e92bd5f2
Faisal Khan <faisalkhanattari35@gmail.com>
2025-10-04 15:55:05 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "sonarqube_public_ip" {
   description = "Public IP of the SonarQube EC2 instance"
-  value       = aws_instance.sonarqube_instance.public_ip
+  value       = aws_instance.this.public_ip
 }
 
 output "sonarqube_instance_id" {
   description = "Instance ID of the SonarQube EC2"
-  value       = aws_instance.sonarqube_instance.id
+  value       = aws_instance.this.id
 }
